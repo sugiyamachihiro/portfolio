9b734132243379ab9f36b27e41501183dfd6a41f
sugiyamachihiro <cccabdy1012@gmail.com>
2019-07-02 06:34:10 +0000

    cat model/controller/view create

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,4 +3,20 @@ class CatsController < ApplicationController
 	def index
 	end
 
+    def new
+    	@cat = Cat.new
+    end
+
+    def create
+    	@cat = Cat.new(cat_params)
+    end
+
+
+
+  private
+
+    def cat_params
+        params.require(:cat).permit(:user_id, :title, :prefecture, :reason, :age, :age_detail, :sex, :kind, :condition, :vaccine, :remark, :individual_corporate, :cat_status)
+    end
+
 end

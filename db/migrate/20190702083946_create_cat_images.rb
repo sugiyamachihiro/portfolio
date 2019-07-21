750be2b672b977806b173f6fd826fef97cd54eda
EC2 Default User <ec2-user@ip-172-31-45-134.us-east-2.compute.internal>
2019-07-21 04:41:42 +0000

    deploy finish

--- db/migrate/20190702083946_create_cat_images.rb
+++ db/migrate/20190702083946_create_cat_images.rb
@@ -1,6 +1,6 @@
 class CreateCatImages < ActiveRecord::Migration[5.2]
   def change
-    create_table :cat_images do |t|
+    create_table :cat_images, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.integer :cat_id
       t.string :image_id
 
